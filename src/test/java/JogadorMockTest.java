d56827a165e22216e4b076f473c8a89fabc6ff2c
Gabriel Wellington <bielwellington@users.noreply.github.com>
2020-11-25 00:07:53 -0300

    Create JogadorMockTest.java
    Realizado teste inicial para o Mock.

---
+++ src/test/java/JogadorMockTest.java
@@ -0,0 +1,45 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(JogadorMock.class)
+public class JogadorMockTest {
+
+    private double valorMenor;
+    private double valorMaior;
+    private double valorMeia;
+    private double valorCheia;
+
+    @Before
+    public void prepareTests(){
+        valorMenor = 9;
+        valorMaior = 21;
+        valorMeia = 10;
+        valorCheia = 20;
+    }
+
+    @Test
+    public void testComprarMeiaVida() throws Exception {
+
+        //arrange
+        JogadorMock compraVida =  new JogadorMock();
+
+        double esperado = 50;
+        double atual;
+
+        //act
+        compraVida.comprarCura(valorCheia);
+
+        atual = compraVida.getTotal();
+
+        //assert
+        Assert.assertEquals(  esperado, atual, 0);
+
+    }
+}
